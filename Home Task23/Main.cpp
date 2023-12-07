d6c94cb500ad454c9eb7822d119c7f181449c9d7
Дмитрий Рындаков <branzino@mail.ru>
2023-12-07 14:59:49 +0500

    Add project files.

---
+++ Home Task23/Main.cpp
@@ -0,0 +1,66 @@
+﻿#include<iostream>
+
+void max_three(int& num1, int& num2, int& num3) {
+	int max = num1;
+	max = max > num2 ? max : num2;
+	max = max > num3 ? max : num3;
+	num1 = num2 = num3= max;
+}
+
+void print_arr(int arr[], const int length) {
+	for (int i = 0; i < length; i++)
+		std::cout << *(arr + i) << ' ';
+	std::cout << std::endl;
+}
+
+int& neg(int arr[], const int length) {
+	int min = 0;
+	for (int i = 0; i < length; i++)
+		if (arr[i] < 0) {
+			min = i;
+			break;
+		}
+	return arr[min];
+}
+
+void zero(int arr[], const int length, int arr2[], const int length2) {
+	for (int i = 0; i < length; i++)			
+		for (int j = 0; j < length2; j++)		
+			if (*(arr + i) == *(arr2 + j))		
+				*(arr2 + j) = 0;
+};
+
+
+
+
+int main() {
+
+	setlocale(LC_ALL, "ru");
+	
+	std::cout << "Задача 1. Максимальное из 3x\n";
+	int A = 23, B = 12, C = 17;
+	max_three(A, B, C);
+	std::cout << A << ' ' << B << ' ' << C << "\n\n";
+	
+	std::cout << "Задача 2. Негативный элемент \n";
+	const int size = 5;
+	int arr[size]{5,12,6,-12,9};
+	std::cout << "Изначальный массив \n";
+	print_arr(arr,size);
+	if (neg(arr, size) < 0)
+		std::cout << "Первый отрицательный элемент -> " << neg(arr, size)<< "\n\n";
+	else
+		std::cout << "Отрицательных элементов в массиве нет. Первый элемент -> "<< neg(arr,size)<<"\n\n";
+
+	std::cout << "Задача 2. Обнуление элементов \n";
+	const int size2 = 5;
+	int arr2[size2]{ 12,5,95,12,3 };
+	std::cout << "Изначальный массив 1 ->\n";
+	print_arr(arr, size);
+	std::cout << "Изначальный массив 2 ->\n";
+	print_arr(arr2, size2);
+	std::cout << "Итоговый массив ->\n";
+	zero(arr,size,arr2,size2);
+	print_arr(arr2, size2);
+	return 0;
+}
---
+++ Home Task23/Test.cpp
@@ -0,0 +1,68 @@
+﻿﻿#include <iostream>
+
+void three_max(int& num1, int& num2, int& num3) {
+	int max = num1;
+	max = max > num2 ? max : num2;
+	max = max > num3 ? max : num3;
+	num1 = max;
+	num2 = max;
+	num3 = max;
+}
+void print_arr(int arr[], const int length) {
+	for (int i = 0; i < length; i++)
+		std::cout << *(arr + i) << ' ';
+	std::cout << std::endl;
+}
+int& neg_element(int arr[], const int length) {
+	int neg = 0;
+	for (int i = 0; i < length; i++)
+		if (*(arr + i) < 0) {
+			neg = i;
+			break;
+		}
+	return arr[neg];
+}
+void zero_repetitions(int arr[], int arr2[], const int length, const int length2) {
+	for (int i = 0; i < length2; i++)
+		if (*(arr + i) == *(arr2 + i))
+			*(arr2 + i) = 0;
+}
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	std::cout << "Задача 1. Присвоение максимума.\n\n";
+	int A = 13, B = 5, C = 6;
+	three_max(A, B, C);
+	std::cout << A << ' ' << B << ' ' << C << std::endl;
+
+
+	std::cout << "\nЗадача 2. Ссылка на элемент массива.\n\n";
+	const int size = 10;
+	int arr[size]{ 10,20,30,5,45,-15,8,9,80,99 };
+	print_arr(arr, size);
+	if (neg_element(arr, size) < 0)
+		std::cout << "Первый отрицательный элемент в массиве -> " << neg_element(arr, size) << std::endl;
+	else
+		std::cout << "В массиве нет отрицательных элементов! Выводим первый элемент -> " << neg_element(arr, size) << std::endl;
+
+	std::cout << "\nЗадача 3. Обнуление дублей в массиве.\n\n";
+
+	const int size2 = 10;
+	int arr2[size2]{ 10,20,30,40,50,60,70,80,90,100 };
+
+	const int size3 = 10;
+	int arr3[size3]{ 10,22,30,42,50,61,70,81,90,101 };
+	std::cout << "Первый массив: \n";
+	print_arr(arr2, size2);
+	std::cout << "Второй массив: \n";
+	print_arr(arr3, size3);
+	zero_repetitions(arr2, arr3, size2, size3);
+	std::cout << "\nМассив после обнуления дублей: \n";
+	print_arr(arr3, size3);
+
+
+
+	return 0;
+}
